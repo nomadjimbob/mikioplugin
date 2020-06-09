9e0ff2d57b380903b3e43738fbd1ebb5c810bd88
James Collins <james.collins@outlook.com.au>
2020-06-10 09:20:51 +1000

    added element

---
+++ syntax/cardbody.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Mikio Syntax Plugin: Card Body
+ *
+ * Syntax:  <CARD-BODY></CARD-BODY>
+ * 
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     James Collins <james.collins@outlook.com.au>
+ */
+ 
+if (!defined('DOKU_INC')) die();
+if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(dirname(__FILE__).'/core.php');
+ 
+class syntax_plugin_mikioplugin_cardbody extends syntax_plugin_mikioplugin_core {
+    public $tag                 = 'card-body';
+    
+    public function render_lexer_enter(Doku_Renderer $renderer, $data) {
+        $renderer->doc .= '<div class="card-body">';
+    }
+
+
+    public function render_lexer_exit(Doku_Renderer $renderer, $data) {
+        $renderer->doc .= '</div>'; 
+    }
+}
+?>
