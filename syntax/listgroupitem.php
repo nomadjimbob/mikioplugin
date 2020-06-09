9bff4e78b60d6995ded197dd93d744897b485caf
James Collins <james.collins@outlook.com.au>
2020-06-10 09:20:58 +1000

    renamed tag

--- syntax/listgroupitem.php
+++ syntax/listgroupitem.php
@@ -2,7 +2,7 @@
 /**
  * Mikio Syntax Plugin: List Group
  *
- * Syntax:  <LISTGROUPITEM></LISTGROUPITEM>
+ * Syntax:  <LISTGROUP-ITEM></LISTGROUP-ITEM>
  * 
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     James Collins <james.collins@outlook.com.au>
@@ -13,7 +13,7 @@ if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(dirname(__FILE__).'/core.php');
  
 class syntax_plugin_mikioplugin_listgroupitem extends syntax_plugin_mikioplugin_core {
-    public $tag                 = 'listgroupitem';
+    public $tag                 = 'listgroup-item';
     
     
     public function render_lexer_enter(Doku_Renderer $renderer, $data) {
