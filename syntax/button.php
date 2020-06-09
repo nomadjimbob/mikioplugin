3aa49f35bd037351d20ceb201751611b2bd2bf2c
James Collins <james.collins@outlook.com.au>
2020-06-10 08:35:08 +1000

    added nowrap option

--- syntax/button.php
+++ syntax/button.php
@@ -2,7 +2,7 @@
 /**
  * Mikio Syntax Plugin: Button
  *
- * Syntax:  <BUTTON [primary|secondary|success|danger|warning|info|light|dark] [url] [target] [newtab] [collapse-id]></BUTTON>
+ * Syntax:  <BUTTON [primary|secondary|success|danger|warning|info|light|dark] [url] [target] [newtab] [collapse-id] [nowrap]></BUTTON>
  * 
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     James Collins <james.collins@outlook.com.au>
@@ -24,12 +24,13 @@ class syntax_plugin_mikioplugin_button extends syntax_plugin_mikioplugin_core {
         'url',
         'target',
         'newtab',
-        'collapse-id'
+        'collapse-id',
+        'nowrap'
     );
     
     
     public function render_lexer_enter(Doku_Renderer $renderer, $data) {
-        $classes = $this->buildClassString($data, array('type', 'size', 'block', 'active', 'disabled'), array('btn-' => array('type', 'size', 'block')));
+        $classes = $this->buildClassString($data, array('type', 'size', 'block', 'active', 'disabled', 'nowrap'), array('btn-' => array('type', 'size', 'block'), 'text-' => 'nowrap'));
 
         $url = '#';
         if(array_key_exists('url', $data) && $data['url'] != '') $url = $this->getLink($data['url']);
