a37131b9c9e52e7344fef9f12f6121ad0e50a145
James Collins <james.collins@outlook.com.au>
2020-06-10 10:59:20 +1000

    added small option

--- syntax/cardfooter.php
+++ syntax/cardfooter.php
@@ -14,15 +14,20 @@ require_once(dirname(__FILE__).'/core.php');
  
 class syntax_plugin_mikioplugin_cardfooter extends syntax_plugin_mikioplugin_core {
     public $tag                 = 'card-footer';
+    public $options             = array('small');
     
     public function render_lexer_enter(Doku_Renderer $renderer, $data) {
         $classes = $this->buildClassString($data);
         
         $renderer->doc .= '<div class="card-footer ' . $classes . '">';
+
+        if(array_key_exists('small', $data) && $data['small'] != false) $renderer->doc .= '<small>';
     }
 
 
     public function render_lexer_exit(Doku_Renderer $renderer, $data) {
+        if(array_key_exists('small', $this->values) && $this->values['small'] == true) $renderer->doc .= '</small>';
+
         $renderer->doc .= '</div>'; 
     }
 }
