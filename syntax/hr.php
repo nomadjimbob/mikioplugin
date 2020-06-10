ef1dcceb991c6d9fa13d38d1f8b499b1f8e42444
James Collins <james.collins@outlook.com.au>
2020-06-10 12:30:01 +1000

    now extends plugin_core

--- syntax/hr.php
+++ syntax/hr.php
@@ -2,7 +2,7 @@
 /**
  * Mikio Syntax Plugin: HR
  *
- * Syntax:  ---- or <HR> will be replaced with the horizontal line element
+ * Syntax:  <HR> or ----
  * 
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     James Collins <james.collins@outlook.com.au>
@@ -10,31 +10,23 @@
  
 if (!defined('DOKU_INC')) die();
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-
+require_once(dirname(__FILE__).'/core.php');
  
-class syntax_plugin_mikioplugin_hr extends DokuWiki_Syntax_Plugin {
-    public function getType(){ return 'substition'; }
-    public function getAllowedTypes() { return array('formatting', 'substition', 'disabled'); }   
-    public function getSort(){ return 238; }
+class syntax_plugin_mikioplugin_hr extends syntax_plugin_mikioplugin_core {
+    public $tag                 = 'hr';
+    public $noEndTag            = true;
     
-    
+
     public function connectTo($mode) {
         $this->Lexer->addSpecialPattern('----', $mode, 'plugin_mikioplugin_'.$this->getPluginComponent());
-        $this->Lexer->addSpecialPattern('<hr>', $mode, 'plugin_mikioplugin_'.$this->getPluginComponent());
+        parent::connectTo($mode);
     }
 
 
-    public function handle($match, $state, $pos, Doku_Handler $handler){
-        return array($match, $state, $pos);
+    public function render_lexer_special(Doku_Renderer $renderer, $data) {
+        $classes = $this->buildClassString($data);
+        
+        $renderer->doc .= '<hr class="' . $classes . '">';
     }
-
-
-    public function render($mode, Doku_Renderer $renderer, $data) {
-        if($mode == 'xhtml') {
-            $renderer->doc .= '<hr>';
-            return true;
-        }
-
-        return false;
-    }   
 }
+?>
