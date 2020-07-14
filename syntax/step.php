8951097dc1a742b5f863b46c284b90f723852062
James Collins <james.collins@outlook.com.au>
2020-07-15 09:30:51 +1000

    cleaned up

--- syntax/step.php
+++ syntax/step.php
@@ -24,8 +24,6 @@ class syntax_plugin_mikioplugin_step extends syntax_plugin_mikioplugin_core {
         $this->addCommonOptions('type');
     }
 
-    // public function getAllowedTypes() {  return array(); }
-
     public function render_lexer_enter(Doku_Renderer $renderer, $data) {
         $classes = $this->buildClass($data, array('complete'));
 
@@ -46,47 +44,5 @@ class syntax_plugin_mikioplugin_step extends syntax_plugin_mikioplugin_core {
         $renderer->doc .= '</div>';
         $renderer->doc .= '</li>';
     }
-
-
-    // public function render_lexer_unmatched(Doku_Renderer $renderer, $data) {
-    //     $items = [];
-    //     $bar = '';
-
-    //     if(preg_match_all('/<(?i:' . $this->tagPrefix . 'step)(.*?)>/s', $data, $match)) {
-    //         if(count($match) >= 1) {
-    //             for($i = 0; $i < count($match[1]); $i++) {
-    //                 $title = '';
-    //                 $text = '';
-    //                 $complete = false;
-
-    //                 if(preg_match('/title=("\w[\w\s]*(?=")|\w+|"[\w\s]*")/is', $match[1][$i], $titleMatch)) {
-    //                     if(count($titleMatch) >= 1) {
-    //                         $title = str_replace("\"", "", $titleMatch[1]);
-    //                     }
-    //                 }
-
-    //                 if(preg_match('/text=("\w[\w\s]*(?=")|\w+|"[\w\s]*")/is', $match[1][$i], $titleMatch)) {
-    //                     if(count($titleMatch) >= 1) {
-    //                         $text = str_replace("\"", "", $titleMatch[1]);
-    //                     }
-    //                 }
-
-    //                 if(preg_match('/ complete /is', $match[1][$i], $titleMatch)) {
-    //                     if(count($titleMatch) >= 1) {
-    //                         $complete = true;
-    //                     }
-    //                 }
-
-    //                 $items[] = array('title' => $title, 'text' => $text, 'complete' => $complete);
-    //             }
-    //         }
-    //     }
-
-    //     foreach($items as $item) {
-    //         $bar .= '<li class="' . $this->elemClass . ' ' . $this->classPrefix . 'step' . ($item['complete'] ? ' ' . $this->classPrefix . 'complete' : '' ) . '"><b>' . $item['title'] .'</b>' . $item['text'] . '</li>';
-    //     }
-
-    //     $renderer->doc .= $bar . '</ul>';
-    // }
 }
 ?>
