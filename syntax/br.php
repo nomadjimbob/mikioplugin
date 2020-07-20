e14e4850a7760bce555b84656ae30820c27b5724
James Collins <james.collins@outlook.com.au>
2020-07-20 20:31:47 +1000

    Added br tag

---
+++ syntax/br.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Mikio Syntax Plugin: BR
+ *
+ * @link        http://github.com/nomadjimbob/mikioplugin
+ * @license     GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author      James Collins <james.collins@outlook.com.au>
+ */
+if (!defined('DOKU_INC')) die();
+if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(dirname(__FILE__).'/core.php');
+ 
+class syntax_plugin_mikioplugin_br extends syntax_plugin_mikioplugin_core {
+    public $tag                 = 'br';
+    public $hasEndTag           = false;
+    
+    public function getPType() { return 'normal'; }
+
+    public function render_lexer_special(Doku_Renderer $renderer, $data) {
+        $renderer->doc .= '<br>';
+    }
+}
+?>
