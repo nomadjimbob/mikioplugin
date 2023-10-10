d454487b5f3f73b23cac8b122dc5222e36c1a432
James Collins <james.collins@outlook.com.au>
2023-10-10 19:38:55 +1000

    added color and text-decoration options

--- syntax/heading.php
+++ syntax/heading.php
@@ -14,16 +14,23 @@ class syntax_plugin_mikioplugin_heading extends syntax_plugin_mikioplugin_core {
     public $tag                 = 'heading';
     public $hasEndTag           = true;
     public $options             = array(
-        'size'  =>  array('type'    => 'choice',
-                          'data'    => array('1', '2', '3', '4', '5', '6'),
-                          'default' => '1'),
+        'color'             => array('type'    => 'color',    'default' => ''),
+        'size'              => array('type'    => 'choice',
+                                     'data'    => array('1', '2', '3', '4', '5', '6'),
+                                     'default' => '1'),
+        'text-decoration'   => array('type'    => 'text',     'default' => '1'),
     );
     // 
     public function getAllowedTypes() { return array('formatting', 'substition', 'disabled'); }
     public function getPType() { return 'normal'; }
     
     public function render_lexer_enter(Doku_Renderer $renderer, $data) {
-        $renderer->doc .= '<h' . $data['size'] . ' class="' . $this->elemClass. ' ' . $this->classPrefix . 'heading ' . $this->classPrefix . 'heading-' . $data['size'] . '">';
+        $styles = $this->buildStyle(array(
+            'color'             => $data['color'],
+            'text-decoration'   => $data['text-decoration'],
+        ), TRUE);
+
+        $renderer->doc .= '<h' . $data['size'] . ' class="' . $this->elemClass. ' ' . $this->classPrefix . 'heading ' . $this->classPrefix . 'heading-' . $data['size'] . '"' . $styles . '>';
     }
 
 
