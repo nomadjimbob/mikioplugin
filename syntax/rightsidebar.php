03ae17ba0b4ab8925a3e4b0cbda47ce9a224676d
James Collins <james.collins@outlook.com.au>
2024-01-14 08:53:01 +1000

    added rightsidebar

---
+++ syntax/rightsidebar.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Mikio Syntax Plugin: Column
+ *
+ * @link        http://github.com/nomadjimbob/mikioplugin
+ * @license     GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author      James Collins <james.collins@outlook.com.au>
+ */
+if (!defined('DOKU_INC')) die();
+if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(dirname(__FILE__).'/core.php');
+
+class syntax_plugin_mikioplugin_rightsidebar extends syntax_plugin_mikioplugin_core {
+    public $tag                 = 'right-sidebar';
+    
+    public function getAllowedTypes()
+    {
+        return array('container', 'formatting', 'substition', 'protected', 'disabled', 'paragraphs');
+    }
+
+    public function getPType() { return 'normal'; }
+    
+    public function render_lexer_enter(Doku_Renderer $renderer, $data) {
+        $renderer->doc .= '<div class="' . $this->elemClass . ' ' . $this->classPrefix . 'right-sidebar">';
+    }
+
+
+    public function render_lexer_exit(Doku_Renderer $renderer, $data) {
+        $renderer->doc .= '</div>'; 
+    }
+}
+?>
